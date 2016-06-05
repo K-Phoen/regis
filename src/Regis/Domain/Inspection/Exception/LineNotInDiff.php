05f5729df853598796fb7f03bf8783c9762ff41b
Kévin Gomez <contact@kevingomez.fr>
2016-06-05 15:06:37 +0200

    Add missing return typehint

--- src/Regis/Domain/Inspection/Exception/LineNotInDiff.php
+++ src/Regis/Domain/Inspection/Exception/LineNotInDiff.php
@@ -6,7 +6,7 @@ namespace Regis\Domain\Inspection\Exception;
 
 class LineNotInDiff extends \RuntimeException
 {
-    public static function line(int $line)
+    public static function line(int $line): LineNotInDiff
     {
         return new static(sprintf('Line %d is not in diff.', $line));
     }
