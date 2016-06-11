e5f5bd26e9eba9f44b55b289b5901d53c38fc7b5
Kévin Gomez <contact@kevingomez.fr>
2016-06-11 13:48:28 +0200

    Add tests for the github reporter

---
+++ tests/Regis/Application/Reporter/GithubTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Regis\Application\Reporter;
+
+use Regis\Application\Model\Github\PullRequest;
+use Regis\Application\Model\Github\ReviewComment;
+use Regis\Application\Model\Violation;
+use Regis\Application\Reporter\Github as GithubReporter;
+use Regis\Github\Client as GithubClient;
+
+class GithubTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var GithubClient */
+    private $client;
+
+    /** @var GithubReporter */
+    private $reporter;
+
+    public function setUp()
+    {
+        $this->client = $this->createMock(GithubClient::class);
+
+        $this->reporter = new GithubReporter($this->client);
+    }
+
+    public function testViolationsAreReportedAsReviewCOmments()
+    {
+        $violation = $this->createMock(Violation::class);
+        $pullRequest = $this->createMock(PullRequest::class);
+
+        $this->client->expects($this->once())
+            ->method('sendComment')
+            ->with($pullRequest, $this->callback(function ($subject) {
+                return $subject instanceof ReviewComment;
+            }));
+
+        $this->reporter->report($violation, $pullRequest);
+    }
+}
