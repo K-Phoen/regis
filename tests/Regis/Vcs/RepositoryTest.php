8750add87b6c9c39acd2a240632423884202bd08
Kévin Gomez <contact@kevingomez.fr>
2016-06-25 22:41:52 +0200

    Add dumb tests for the Vcs\Repository class

---
+++ tests/Regis/Vcs/RepositoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Regis\Vcs;
+
+use Gitonomy\Git as Gitonomy;
+
+use Regis\Application\Model\Git as Model;
+use Regis\Vcs\Repository;
+
+class RepositoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUpdate()
+    {
+        $gitonomyRepository = $this->getMockBuilder(Gitonomy\Repository::class)->disableOriginalConstructor()->getMock();
+        $repository = new Repository($gitonomyRepository);
+
+        $gitonomyRepository->expects($this->once())
+            ->method('run')
+            ->with('fetch');
+
+        $repository->update();
+    }
+
+    public function testGetDiff()
+    {
+        $gitonomyRepository = new Gitonomy\Repository(APP_ROOT_DIR);
+        $repository = new Repository($gitonomyRepository);
+        $revisions = new Model\Revisions('5445fc28ee3b6c01194f7df770bb79783a16af45', '17ed7410514072f74352fd0a91586d7684eef886');
+
+        $this->assertInstanceOf(Model\Diff::class, $repository->getDiff($revisions));
+    }
+}
