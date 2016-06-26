c7abd46c76ad2b2c9c4b6e0fdb44efca5c8ce949
Kévin Gomez <contact@kevingomez.fr>
2016-06-26 14:23:51 +0200

    Add tests for the InMemoryRepositories repository

---
+++ tests/Regis/Application/Repository/InMemoryRepositoriesTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Regis\Application\Repository;
+
+use Regis\Application\Entity;
+use Regis\Application\Repository\InMemoryRepositories;
+
+class InMemoryRepositoriesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFindAllAGeneratorToTheRepositories()
+    {
+        $repo = new InMemoryRepositories([
+            'some identifier' => [
+                'secret' => 'shared secret',
+            ]
+        ]);
+
+        $this->assertInstanceOf(\Generator::class, $result = $repo->findAll());
+        $this->assertCount(1, iterator_to_array($result));
+    }
+
+    /**
+     * @expectedException \Regis\Application\Repository\Exception\NotFound
+     */
+    public function testFindThrowAnExceptionIfTheEntityDoesNotExist()
+    {
+        $repo = new InMemoryRepositories([
+            'some identifier' => [
+                'secret' => 'shared secret',
+            ]
+        ]);
+
+        $repo->find('some identifier that does not exist');
+    }
+
+    public function testFindReturnsTheEntityIfItExists()
+    {
+        $repo = new InMemoryRepositories([
+            'some identifier' => [
+                'secret' => 'shared secret',
+            ]
+        ]);
+
+        $entity = $repo->find('some identifier');
+
+        $this->assertInstanceOf(Entity\Github\Repository::class, $entity);
+        $this->assertSame('some identifier', $entity->getIdentifier());
+        $this->assertSame('shared secret', $entity->getSharedSecret());
+    }
+
+    public function testARepositoryCanBeSaved()
+    {
+        $repo = new InMemoryRepositories([]);
+
+        $entity = new Entity\Github\Repository('some identifier', 'shared secret');
+        $repo->save($entity);
+
+        $this->assertSame($entity, $repo->find('some identifier'));
+    }
+}
