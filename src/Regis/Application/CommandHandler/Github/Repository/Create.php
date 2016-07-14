577f0aea545d3a53f065a453fa7eadd1a85e034e
Kévin Gomez <contact@kevingomez.fr>
2016-07-14 16:08:37 +0200

    Do not try to create a repository that already exist

--- src/Regis/Application/CommandHandler/Github/Repository/Create.php
+++ src/Regis/Application/CommandHandler/Github/Repository/Create.php
@@ -8,6 +8,7 @@ use Regis\Application\Command;
 use Regis\Application\Random\Generator as RandomGenerator;
 use Regis\Domain\Entity;
 use Regis\Domain\Repository\Repositories;
+use Regis\Domain\Repository\Exception;
 
 class Create
 {
@@ -22,6 +23,12 @@ class Create
 
     public function handle(Command\Github\Repository\Create $command)
     {
+        try {
+            return $this->repositoriesRepo->find($command->getIdentifier());
+        } catch (Exception\NotFound $e) {
+            // the repository does not exist yet, we can continue and create it.
+        }
+
         $sharedSecret = $command->getSharedSecret() ?: $this->randomGenerator->randomString();
         $repository = new Entity\Github\Repository($command->getOwner(), $command->getIdentifier(), $sharedSecret);
 
