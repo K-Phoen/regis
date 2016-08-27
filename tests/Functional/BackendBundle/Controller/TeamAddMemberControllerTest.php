f70fdca5319c8f06a9ea754cfa9ca0572553752c
Kévin Gomez <contact@kevingomez.fr>
2016-08-27 18:13:27 +0200

    Add a few functional tests for the user search controller

---
+++ tests/Functional/BackendBundle/Controller/TeamAddMemberControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Functional\BackendBundle\Controller;
+
+use Tests\Functional\WebTestCase;
+
+class TeamAddMemberControllerTest extends WebTestCase
+{
+    public function testUsersCanNotBeSearchedByAnonymous()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/backend/teams/users');
+
+        $this->assertTrue($client->getResponse()->isRedirect('/login'));
+    }
+
+    /**
+     * @dataProvider searchProvider
+     */
+    public function testUserCanBeSearched($search, $expectedUserEmails)
+    {
+        $client = static::createClient();
+        $this->logIn($client, 'user');
+
+        $client->request('GET', '/backend/teams/users', [
+            'q' => $search
+        ]);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('application/json', $client->getResponse()->headers->get('Content-Type'));
+
+        $results = json_decode($client->getResponse()->getContent(), true);
+        $emails = array_column($results['users'], 'email');
+
+        $this->assertEquals($expectedUserEmails, $emails);
+    }
+
+    public function searchProvider()
+    {
+        return [
+            [ '', [] ],
+            [ 'admin', ['admin@admin'] ],
+            [ 'ad', ['admin@admin'] ],
+
+            [ 'us', ['user@foo.org'] ],
+            [ 'foo.org', ['user@foo.org'] ],
+        ];
+    }
+}
