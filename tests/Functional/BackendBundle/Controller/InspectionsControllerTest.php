bfc5b9d451d4ca2d52baa80e44d99170aebfca05
Kévin Gomez <contact@kevingomez.fr>
2016-09-11 22:19:28 +0200

    Add a few tests for the inspections controller

---
+++ tests/Functional/BackendBundle/Controller/InspectionsControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Functional\BackendBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+
+use Regis\Domain\Entity;
+use Tests\Functional\WebTestCase;
+
+class InspectionsControllerTest extends WebTestCase
+{
+    public function testViewingAnInspectionRequiresAuthentication()
+    {
+        $client = static::createClient();
+        $inspection = $this->findInspection($client);
+
+        $client->request('GET', '/backend/inspections/'.$inspection->getId());
+
+        $this->assertTrue($client->getResponse()->isRedirect('/login'));
+    }
+
+    public function testThatAnAuthorizedUserCanAccessAnInspectionPage()
+    {
+        $client = static::createClient();
+        $inspection = $this->findInspection($client);
+
+        $this->logIn($client, 'user');
+
+        $crawler = $client->request('GET', '/backend/inspections/'.$inspection->getId());
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('.main:contains("Inspection")')->count()
+        );
+    }
+
+    private function findInspection(Client $client): Entity\Inspection
+    {
+        $repo = $client->getContainer()->get('doctrine.orm.default_entity_manager')->getRepository(Entity\Inspection::class);
+
+        return $repo->findOneBy([]);
+    }
+}
