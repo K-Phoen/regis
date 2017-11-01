317f0737d2db21ddde907e47fabb9da53ecb30f6
Kévin Gomez <contact@kevingomez.fr>
2017-11-01 15:54:37 +0100

    Do not use ircmaxell/random-lib as random_bytes() does the job

--- src/Regis/Infrastructure/RandomLib/Generator.php
+++ src/Regis/Infrastructure/RandomLib/Generator.php
@@ -8,17 +8,8 @@ use Regis\Application\Random\Generator as RandomGenerator;
 
 class Generator implements RandomGenerator
 {
-    private $generator;
-
-    public function __construct()
-    {
-        $factory = new \RandomLib\Factory();
-
-        $this->generator = $factory->getMediumStrengthGenerator();
-    }
-
     public function randomString(int $length = 24): string
     {
-        return $this->generator->generateString($length);
+        return random_bytes($length);
     }
 }
