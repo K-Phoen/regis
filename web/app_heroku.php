efbfd24f6e531be58d1f6fb2b5407006c42f5510
Kévin Gomez <contact@kevingomez.fr>
2017-11-26 18:08:25 +0100

    Add heroku-specific front-controller

--- web/app_heroku.php
+++ web/app_heroku.php
@@ -4,11 +4,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../app/autoload.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel(getenv('SYMFONY_ENV') ?: 'prod', getenv('SYMFONY_DEBUG') ?: false);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+
+// Heroku-specific. See https://devcenter.heroku.com/articles/getting-started-with-symfony#trusting-the-load-balancer
+Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR')));
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_HOST, null);
 
 $response = $kernel->handle($request);
 $response->send();
