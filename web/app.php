f68756b67bd8c97014b71d293bc902f467dee17f
Kévin Gomez <contact@kevingomez.fr>
2017-11-26 18:01:24 +0100

    Trust heroku's load-balancer

--- web/app.php
+++ web/app.php
@@ -9,6 +9,12 @@ $kernel = new AppKernel('prod', false);
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
+
+// Heroku-specific. See https://devcenter.heroku.com/articles/getting-started-with-symfony#trusting-the-load-balancer
+Request::setTrustedProxies(array($request->server->get('REMOTE_ADDR')));
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_HOST, null);
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
